fffe170dc88d6fee386b07c32ec923a3ba059714
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2023-11-29 22:58:53 +0900

    Update main_server.py

--- 003 Code/Server/main_server.py
+++ 003 Code/Server/main_server.py
@@ -1,4 +1,5 @@
-from views import bp_userjoin, bp_run_face, bp_mode, bp_temp_pw
+import json
+from views import bp_join_user, bp_run_voice, bp_run_face, bp_mode, bp_temp_pw
 from flask import Flask
 from flask import Flask, request
 from werkzeug.utils import secure_filename
@@ -16,38 +17,55 @@ from torch.nn import DataParallel
 from datetime import datetime
 from retinaface import RetinaFace
 from itertools import combinations
+from fcm import fcm_data, fcm_noti
 import posixpath
+from voice_test import voice_test
 
 app = Flask(__name__)
 
-app.register_blueprint(bp_userjoin.bp)
+
+# Firebase 앱 초기화(initialize_app) 함수 호출
+PROJECT_ID = "framerun-cloud"
+# (키 이름 ) 부분에 본인의 키이름을 적어주세요.
+cred = credentials.Certificate(
+    키 값 json 파일 경로)
+default_app = firebase_admin.initialize_app(
+    cred, {'storageBucket': f"{PROJECT_ID}.appspot.com"})
+
+app.register_blueprint(bp_join_user.bp)
+app.register_blueprint(bp_run_voice.bp)
 app.register_blueprint(bp_run_face.bp)
 app.register_blueprint(bp_mode.bp)
 app.register_blueprint(bp_temp_pw.bp)
-
-# Firebase 앱 초기화(initialize_app) 함수 호출
-PROJECT_ID = "프로젝트 ID"
-# (키 이름 ) 부분에 본인의 키이름을 적어주세요.
-cred = credentials.Certificate(
-    "firebase-adminsdk json파일 경로")
-default_app = firebase_admin.initialize_app(
-    cred, {'storageBucket': f"{PROJECT_ID}.appspot.com"})
 
 @app.route('/')
 def home_screen():
     return "Framerun"
 
+# 어플이 새로 켜질때마다 보내는 token 값을 받는 서버 코드
 @app.route('/token', methods=['POST'])
 def get_token():
-    response = request.get_json()
-    with open(r"C:\Framerun\webserver\token.txt", "w") as file:
-        file.write(response['data'])  # 받은 token 값을 파일에 저장하기
+    token = request.get_json()
+    with open("c:/Framerun/webserver/token.json", "w") as file:
+        json.dump(token, file)  # 받은 token 값을 파일에 저장하기
+        print("save token done")
     return "save token done"
+
+@app.route('/fcm_send', methods=['POST', 'GET'])
+def fcm_send():
+    
+    filename = request.args.get('file_name')
+    
+    fcm_data(filename)  # fcm.py
+    return ('disapproval')                       
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
 
-if __name__ == '__userjoin__':
+if __name__ == '__join_user__':
+    app.run('0.0.0.0', port=5000, debug=True)
+if __name__ == '__run_voice__':
     app.run('0.0.0.0', port=5000, debug=True)
 if __name__ == '__run_face__':
     app.run('0.0.0.0', port=5000, debug=True)
