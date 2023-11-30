705aac81734c50c07900a4a8f74192deb0701ade
longgani <123061106+longgani@users.noreply.github.com>
2023-11-30 11:16:24 +0900

    Delete 003 Code/App/main directory

--- 003 Code/App/main/app/src/main/java/com/example/framerunappfinal/CheckVisitor/IntruderDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.framerunappfinal.CheckVisitor
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Intruder::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun intruderDao(): IntruderDao
-}
